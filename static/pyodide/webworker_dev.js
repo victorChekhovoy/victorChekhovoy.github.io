4f87bdc674378c8cc12d9eeb781df0591ee65344
victorChekhovoy <viktor.chekhovoi@gmail.com>
2021-04-15 18:02:26 -0500

    adding support for an npm library

---
+++ static/pyodide/webworker_dev.js
@@ -0,0 +1,24 @@
+self.languagePluginUrl = './'
+importScripts('./pyodide.js')
+
+onmessage = async function(e) {
+  await languagePluginLoader;
+  const data = e.data;
+  for (let key of Object.keys(data)) {
+    if (key !== 'python') {
+      // Keys other than python must be arguments for the python script.
+      // Set them on self, so that `from js import key` works.
+      self[key] = data[key];
+    }
+  }
+
+  try {
+    self.postMessage(
+        {results : await self.pyodide.runPythonAsync(data.python)});
+  } catch (e) {
+    // if you prefer messages with the error
+    self.postMessage({error : e.message});
+    // if you prefer onerror events
+    // setTimeout(() => { throw err; });
+  }
+}
